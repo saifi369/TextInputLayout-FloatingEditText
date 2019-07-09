0a4457ed9bb6d1accffbb69373fefda1d472f7d5
M Saif Ullah <chsaif369@gmail.com>
2019-07-10 03:37:12 +0500

    Validate Email & Password with Regular Expressions (Regex)

--- app/src/main/java/com/saifi369/firebaseauthapp/MainActivity.java
+++ app/src/main/java/com/saifi369/firebaseauthapp/MainActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextInputLayout mEmailLayout,mPasswordLayout,mUsernameLayout;
+    private TextInputLayout mEmailLayout, mPasswordLayout, mUsernameLayout;
     private Button mBtnSignin;
 
     @Override
@@ -22,11 +23,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mEmailLayout=findViewById(R.id.et_email);
-        mPasswordLayout=findViewById(R.id.et_password);
-        mUsernameLayout=findViewById(R.id.et_username);
-        mBtnSignin=findViewById(R.id.btn_validate);
-        
+        initViews();
+
         mBtnSignin.setOnClickListener(this::submitData);
 
         mEmailLayout.setErrorTextColor(ColorStateList.valueOf(Color.BLUE));
@@ -36,47 +34,65 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private boolean validateEmailAddress(){
+    private void initViews() {
+        mEmailLayout = findViewById(R.id.et_email);
+        mPasswordLayout = findViewById(R.id.et_password);
+        mUsernameLayout = findViewById(R.id.et_username);
+        mBtnSignin = findViewById(R.id.btn_validate);
+    }
 
-        String email=mEmailLayout.getEditText().getText().toString().trim();
+    private boolean validateEmailAddress() {
 
-        if(email.isEmpty()){
+        String email = mEmailLayout.getEditText().getText().toString().trim();
+
+        if (email.isEmpty()) {
             mEmailLayout.setError("Email is required. Can't be empty.");
             return false;
-        }else {
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailLayout.setError("Invalid Email. Enter valid email address.");
+            return false;
+        } else {
             mEmailLayout.setError(null);
             return true;
         }
     }
 
-    private boolean validatePassword(){
+    private boolean validatePassword() {
 
-        String password=mPasswordLayout.getEditText().getText().toString().trim();
+        String password = mPasswordLayout.getEditText().getText().toString().trim();
 
-        if(password.isEmpty()){
+        if (password.isEmpty()) {
             mPasswordLayout.setError("Password is required. Can't be empty.");
             return false;
-        }else if(password.length()<8){
-            mPasswordLayout.setError("Password is short. 8 Characters are required.");
+        } else if (!Utils.PASSWORD_UPPERCASE_PATTERN.matcher(password).matches()) {
+            mPasswordLayout.setError("Password is weak. Uppercase character required.");
             return false;
-        }
-        else {
+        } else if (!Utils.PASSWORD_LOWERCASE_PATTERN.matcher(password).matches()) {
+            mPasswordLayout.setError("Password is weak. Lowercase character required.");
+            return false;
+        } else if (!Utils.PASSWORD_NUMBER_PATTERN.matcher(password).matches()) {
+            mPasswordLayout.setError("Password is weak. Minimum 1 number is required.");
+            return false;
+        } else if (!Utils.PASSWORD_SPECIAL_CHARACTER_PATTERN.matcher(password).matches()) {
+            mPasswordLayout.setError("Password is weak. Minimum 1 Special character is required.");
+            return false;
+        } else {
             mPasswordLayout.setError(null);
             return true;
         }
     }
 
-    private boolean validateUsername(){
+    private boolean validateUsername() {
 
-        String username=mUsernameLayout.getEditText().getText().toString().trim();
+        String username = mUsernameLayout.getEditText().getText().toString().trim();
 
-        if(username.isEmpty()){
+        if (username.isEmpty()) {
             mUsernameLayout.setError("Username is required. Can't be empty.");
             return false;
-        }else if(username.length()<10){
+        } else if (username.length() < 10) {
             mUsernameLayout.setError("Username is short. 10 Characters are required.");
             return false;
-        }else{
+        } else {
             mUsernameLayout.setError(null);
             return true;
         }
@@ -85,12 +101,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void submitData(View view) {
 
-        if(!validateEmailAddress() | !validatePassword() | !validateUsername()){
+        if (!validateEmailAddress() | !validatePassword() | !validateUsername()) {
             return;
         }
 
-        String data = mEmailLayout.getEditText().getText().toString().trim() +"\n"+
-                mUsernameLayout.getEditText().getText().toString().trim() + "\n"+
+        String data = mEmailLayout.getEditText().getText().toString().trim() + "\n" +
+                mUsernameLayout.getEditText().getText().toString().trim() + "\n" +
                 mPasswordLayout.getEditText().getText().toString();
 
         Toast.makeText(this, data, Toast.LENGTH_LONG).show();
